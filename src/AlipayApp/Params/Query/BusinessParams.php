05ae08b512ef353c19c8d31f63b1b34b64b7d7ee
宇润 <admin@yurunsoft.com>
2017-08-17 09:55:14 +0800

    新增统一收单线下交易查询

---
+++ src/AlipayApp/Params/Query/BusinessParams.php
@@ -0,0 +1,20 @@
+<?php
+namespace Yurun\PaySDK\AlipayApp\Params\Query;
+
+class BusinessParams
+{
+	use \Yurun\PaySDK\Traits\JSONParams;
+
+	/**
+	 * 订单支付时传入的商户订单号,和支付宝交易号不能同时为空。 
+	 * trade_no,out_trade_no如果同时存在优先取trade_no
+	 * @var string
+	 */
+	public $out_trade_no;
+
+	/**
+	 * 支付宝交易号，和商户订单号不能同时为空
+	 * @var string
+	 */
+	public $trade_no;
+}
---
+++ src/AlipayApp/Params/Query/Request.php
@@ -0,0 +1,35 @@
+<?php
+namespace Yurun\PaySDK\AlipayApp\Params\Query;
+
+use \Yurun\PaySDK\AlipayRequestBase;
+use \Yurun\PaySDK\AlipayApp\Page\Params\Pay\BusinessParams;
+
+class Request extends AlipayRequestBase
+{
+	/**
+	 * 接口名称
+	 * @var string
+	 */
+	public $method = 'alipay.trade.query';
+
+	/**
+	 * 详见：https://docs.open.alipay.com/common/105193
+	 * @var string
+	 */
+	public $app_auth_token;
+
+	/**
+	 * 业务请求参数
+	 * 参考https://docs.open.alipay.com/common/105901
+	 * @var \Yurun\PaySDK\AlipayApp\Params\Query\BusinessParams
+	 */
+	public $businessParams;
+
+	public function __construct()
+	{
+		$this->businessParams = new BusinessParams;
+		$this->_method = 'GET';
+		$this->_isSyncVerify = true;
+		$this->_syncResponseName = 'alipay_trade_query_response';
+	}
+}
