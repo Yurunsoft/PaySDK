ef12b0aea392b9241a312c87e0e2d9cbed5a1be5
宇润 <admin@yurunsoft.com>
2017-09-12 17:32:40 +0800

    提交刷卡支付 提交订单查询

---
+++ src/Weixin/Micropay/Params/Pay/Request.php
@@ -0,0 +1,32 @@
+<?php
+namespace Yurun\PaySDK\Weixin\Micropay\Params\Pay;
+
+use \Yurun\PaySDK\Weixin\Params\PayRequestBase;
+use \Yurun\PaySDK\Weixin\Micropay\Params\SceneInfo;
+
+class Request extends PayRequestBase
+{
+	/**
+	 * 接口名称
+	 * @var string
+	 */
+	public $_apiMethod = 'pay/micropay';
+
+	/**
+	 * 扫码支付授权码，设备读取用户微信中的条码或者二维码信息
+	 * @var string
+	 */
+	public $auth_code;
+
+	/**
+	 * 场景信息
+	 * @var \Yurun\PaySDK\Weixin\Micropay\Params\SceneInfo
+	 */
+	public $scene_info;
+
+	public function __construct()
+	{
+		$this->scene_info = new SceneInfo;
+		parent::__construct();
+	}
+}
---
+++ test/Weixin/orderQuery.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\Weixin\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+$params->mch_id = $GLOBALS['PAY_CONFIG']['mch_id'];
+$params->key = $GLOBALS['PAY_CONFIG']['key'];
+$params->certPath = $GLOBALS['PAY_CONFIG']['certPath'];
+$params->keyPath = $GLOBALS['PAY_CONFIG']['keyPath'];
+
+// SDK实例化，传入公共配置
+$sdk = new \Yurun\PaySDK\Weixin\SDK($params);
+
+$request = new \Yurun\PaySDK\Weixin\OrderQuery\Request;
+$request->transaction_id = '4004612001201709121702392532';
+
+var_dump($sdk->execute($request));
