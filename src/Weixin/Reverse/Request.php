d21bc97d0b56a68f4344617caa34ef060d7df9cc
宇润 <admin@yurunsoft.com>
2017-09-13 09:46:09 +0800

    新增微信关闭订单、取消订单

---
+++ src/Weixin/Reverse/Request.php
@@ -0,0 +1,26 @@
+<?php
+namespace Yurun\PaySDK\Weixin\Reverse;
+
+use \Yurun\PaySDK\WeixinRequestBase;
+
+class Request extends WeixinRequestBase
+{
+	/**
+	 * 接口名称
+	 * @var string
+	 */
+	public $_apiMethod = 'secapi/pay/reverse';
+
+	/**
+	 * 微信订单号，与商户订单号二选一
+	 * @var string
+	 */
+	public $transaction_id;
+
+	/**
+	 * 商户订单号与微信订单号二选一
+	 * @var string
+	 */
+	public $out_trade_no;
+
+}
