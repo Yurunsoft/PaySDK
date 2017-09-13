6b6cb306b6ea1eb0fa1f35b2e8f4e613062e8e37
宇润 <admin@yurunsoft.com>
2017-09-13 09:58:11 +0800

    新增微信下载对账单

---
+++ test/Weixin/downloadBill.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\Weixin\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+$params->mch_id = $GLOBALS['PAY_CONFIG']['mch_id'];
+$params->key = $GLOBALS['PAY_CONFIG']['key'];
+
+// SDK实例化，传入公共配置
+$sdk = new \Yurun\PaySDK\Weixin\SDK($params);
+
+$request = new \Yurun\PaySDK\Weixin\DownloadBill\Request;
+$request->bill_date = '20170912';
+$request->bill_type = 'ALL';
+
+// 下载对账单成功时候返回csv格式数据，失败返回xml数据，请自行判断是否成功
+var_dump($sdk->execute($request, ''));
