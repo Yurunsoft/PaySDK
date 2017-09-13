481d78ecdd2488530149efcf86672b541c873b56
宇润 <admin@yurunsoft.com>
2017-09-13 09:49:27 +0800

    新增微信支付查询退款

---
+++ test/Weixin/refundQuery.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\Weixin\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+$params->mch_id = $GLOBALS['PAY_CONFIG']['mch_id'];
+$params->key = $GLOBALS['PAY_CONFIG']['key'];
+
+// SDK实例化，传入公共配置
+$sdk = new \Yurun\PaySDK\Weixin\SDK($params);
+
+$request = new \Yurun\PaySDK\Weixin\RefundQuery\Request;
+$request->transaction_id = '4004612001201709121684151520';
+
+var_dump($sdk->execute($request));
