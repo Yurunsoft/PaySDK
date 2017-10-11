c3b4d66f8a7c475c34fedb49bd1dd7395978340c
宇润 <admin@yurunsoft.com>
2017-10-11 16:04:31 +0800

    新增支付宝统一收单交易退款查询

---
+++ src/AlipayApp/Params/RefundQuery/Request.php
@@ -0,0 +1,35 @@
+<?php
+namespace Yurun\PaySDK\AlipayApp\Params\RefundQuery;
+
+use \Yurun\PaySDK\AlipayRequestBase;
+use \Yurun\PaySDK\AlipayApp\Params\RefundQuery\BusinessParams;
+
+class Request extends AlipayRequestBase
+{
+	/**
+	 * 接口名称
+	 * @var string
+	 */
+	public $method = 'alipay.trade.fastpay.refund.query';
+
+	/**
+	 * 详见：https://docs.open.alipay.com/common/105193
+	 * @var string
+	 */
+	public $app_auth_token;
+
+	/**
+	 * 业务请求参数
+	 * 参考https://docs.open.alipay.com/api_1/alipay.trade.fastpay.refund.query/
+	 * @var \Yurun\PaySDK\AlipayApp\Params\RefundQuery\BusinessParams
+	 */
+	public $businessParams;
+
+	public function __construct()
+	{
+		$this->businessParams = new BusinessParams;
+		$this->_method = 'GET';
+		$this->_isSyncVerify = true;
+		$this->_syncResponseName = 'alipay_trade_fastpay_refund_query_response';
+	}
+}
---
+++ test/AlipayApp/refund_query.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 支付宝统一收单交易退款查询Demo
+ */
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\AlipayApp\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+$params->appPublicKey = $GLOBALS['PAY_CONFIG']['publicKey'];
+$params->appPrivateKey = $GLOBALS['PAY_CONFIG']['privateKey'];
+// $params->appPrivateKeyFile = ''; // 证书文件，如果设置则这个优先使用
+$params->apiDomain = 'https://openapi.alipaydev.com/gateway.do'; // 设为沙箱环境，如正式环境请把这行注释
+
+// SDK实例化，传入公共配置
+$pay = new \Yurun\PaySDK\AlipayApp\SDK($params);
+
+// 支付接口
+$request = new \Yurun\PaySDK\AlipayApp\Params\RefundQuery\Request;
+$request->businessParams->trade_no = '';
+$request->businessParams->out_trade_no = '';
+$request->businessParams->out_request_no = '';
+
+// 调用接口
+var_dump($pay->execute($request));
