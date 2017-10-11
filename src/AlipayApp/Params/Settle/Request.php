81d1b251c3b87b4a7ca2ae30fe47ddac999b53ea
宇润 <admin@yurunsoft.com>
2017-10-11 15:08:58 +0800

    新增支付宝统一收单交易结算接口

---
+++ src/AlipayApp/Params/Settle/Request.php
@@ -0,0 +1,35 @@
+<?php
+namespace Yurun\PaySDK\AlipayApp\Params\Settle;
+
+use \Yurun\PaySDK\AlipayRequestBase;
+use \Yurun\PaySDK\AlipayApp\Params\Settle\BusinessParams;
+
+class Request extends AlipayRequestBase
+{
+	/**
+	 * 接口名称
+	 * @var string
+	 */
+	public $method = 'alipay.trade.order.settle';
+
+	/**
+	 * 详见：https://docs.open.alipay.com/common/105193
+	 * @var string
+	 */
+	public $app_auth_token;
+
+	/**
+	 * 业务请求参数
+	 * 参考https://docs.open.alipay.com/api_1/alipay.trade.order.settle/
+	 * @var \Yurun\PaySDK\AlipayApp\Params\Settle\BusinessParams
+	 */
+	public $businessParams;
+
+	public function __construct()
+	{
+		$this->businessParams = new BusinessParams;
+		$this->_method = 'GET';
+		$this->_isSyncVerify = true;
+		$this->_syncResponseName = 'alipay_trade_order_settle_response';
+	}
+}
