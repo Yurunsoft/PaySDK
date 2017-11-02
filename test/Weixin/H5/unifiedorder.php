a13be6e42cc00dfe8533428ecfdd1b89cc2f58a0
宇润 <admin@yurunsoft.com>
2017-11-02 14:21:54 +0800

    完善微信h5支付测试代码

--- test/Weixin/H5/unifiedorder.php
+++ test/Weixin/H5/unifiedorder.php
@@ -12,14 +12,18 @@ $params->key = $GLOBALS['PAY_CONFIG']['key'];
 
 // SDK实例化，传入公共配置
 $pay = new \Yurun\PaySDK\Weixin\SDK($params);
-
 // 支付接口
 $request = new \Yurun\PaySDK\Weixin\H5\Params\Pay\Request;
 $request->body = 'test'; // 商品描述
 $request->out_trade_no = 'test' . mt_rand(10000000,99999999); // 订单号
 $request->total_fee = 1; // 订单总金额，单位为：分
-$request->spbill_create_ip = '127.0.0.1'; // 客户端ip
+$request->spbill_create_ip = '127.0.0.1'; // 客户端ip，必须传正确的用户ip，否则会报错
 $request->notify_url = $GLOBALS['PAY_CONFIG']['pay_notify_url']; // 异步通知地址
+$request->scene_info = new \Yurun\PaySDK\Weixin\H5\Params\SceneInfo;
+$request->scene_info->type = 'Wap'; // 可选值：IOS、Android、Wap
+// 下面参数根据type不同而不同
+$request->scene_info->wap_url = 'https://baidu.com';
+$request->scene_info->wap_name = 'test';
 
 // 调用接口
 $result = $pay->execute($request);
