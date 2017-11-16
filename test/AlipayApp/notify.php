eaf904b1affeff516f45678d6d16267b7f4ba0d9
宇润 <admin@yurunsoft.com>
2017-11-16 10:23:07 +0800

    修复支付宝应用签名验签问题

--- test/AlipayApp/notify.php
+++ test/AlipayApp/notify.php
@@ -12,7 +12,7 @@ $params->appPrivateKey = $GLOBALS['PAY_CONFIG']['privateKey'];
 // SDK实例化，传入公共配置
 $pay = new \Yurun\PaySDK\AlipayApp\SDK($params);
 
-$content = var_export($_POST, true) . PHP_EOL . 'verify:' . $pay->verifyCallback($_POST);
+$content = var_export($_POST, true) . PHP_EOL . 'verify:' . var_export($pay->verifyCallback($_POST), true);
 
 file_put_contents(__DIR__ . '/notify_result.txt', $content);
 
