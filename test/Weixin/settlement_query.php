5aa7dc8818b4152e3b6a209901c1ec36d55bcbc8
宇润 <admin@yurunsoft.com>
2017-11-20 13:26:03 +0800

    新增微信查询结算资金接口

---
+++ test/Weixin/settlement_query.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 微信支付查询结算资金Demo
+ */
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\Weixin\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+$params->mch_id = $GLOBALS['PAY_CONFIG']['mch_id'];
+$params->key = $GLOBALS['PAY_CONFIG']['key'];
+
+// SDK实例化，传入公共配置
+$sdk = new \Yurun\PaySDK\Weixin\SDK($params);
+
+$request = new \Yurun\PaySDK\Weixin\SettlementQuery\Request;
+$request->usetag  = 1;
+$request->offset = 1;
+$request->limit = 10;
+$request->date_start = '20170801';
+$request->date_end = '20170830';
+
+$result = $sdk->execute($request);
+
+var_dump('result:', $result);
+
+var_dump('success:', $sdk->checkResult());
+
+var_dump('error:', $sdk->getError(), 'error_code:', $sdk->getErrorCode());
