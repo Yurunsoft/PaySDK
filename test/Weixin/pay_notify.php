674f64603db8aeb8cd75728768c0987f608db38f
宇润 <admin@yurunsoft.com>
2017-11-21 17:14:38 +0800

    调整微信通知写法

--- test/Weixin/pay_notify.php
+++ test/Weixin/pay_notify.php
@@ -24,7 +24,7 @@ class PayNotify extends \Yurun\PaySDK\Weixin\Notify\Pay
 		// 支付成功处理，一般做订单处理
 		file_put_contents(__DIR__ . '/notify_result.txt', date('Y-m-d H:i:s') . ':' . var_export($this->data, true));
 		// 告诉微信我处理过了，不要再通过了
-		$this->reply('SUCCESS', 'OK');
+		$this->reply(true, 'OK');
 	}
 }
 $payNotify = new PayNotify;
