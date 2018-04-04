54aaee5ed90b92e58f5358ced444b4a08aa98de5
宇润 <admin@yurunsoft.com>
2018-04-04 11:20:28 +0800

    新增支付宝app支付服务端支持

---
+++ test/AlipayApp/app_pay.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 支付宝手机App支付Demo
+ */
+require __DIR__ . '/common.php';
+
+// 公共配置
+$params = new \Yurun\PaySDK\AlipayApp\Params\PublicParams;
+$params->appID = $GLOBALS['PAY_CONFIG']['appid'];
+//$params->sign_type = 'RSA2'; // 默认就是RSA2
+$params->appPrivateKey = $GLOBALS['PAY_CONFIG']['privateKey'];
+// $params->appPrivateKeyFile = ''; // 证书文件，如果设置则这个优先使用
+$params->apiDomain = 'https://openapi.alipaydev.com/gateway.do'; // 设为沙箱环境，如正式环境请把这行注释
+
+// SDK实例化，传入公共配置
+$pay = new \Yurun\PaySDK\AlipayApp\SDK($params);
+
+// 支付接口
+$request = new \Yurun\PaySDK\AlipayApp\App\Params\Pay\Request;
+$request->notify_url = $GLOBALS['PAY_CONFIG']['notify_url']; // 支付后通知地址（作为支付成功回调，这个可靠）
+$request->businessParams->out_trade_no = 'test' . mt_rand(10000000,99999999); // 商户订单号
+$request->businessParams->total_amount = 0.01; // 价格
+$request->businessParams->subject = '小米手机9黑色陶瓷尊享版'; // 商品标题
+
+// 处理
+$pay->prepareExecute($request, $url, $data);
+
+echo $url; // 输出的是可以让app直接请求的url
