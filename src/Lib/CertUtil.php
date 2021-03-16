9219ce7bb38ad582ec38ebb8998f22cb8afd8839
run0817 <37306014+run0817@users.noreply.github.com>
2021-03-16 18:33:53 +0800

    Update CertUtil.php

--- src/Lib/CertUtil.php
+++ src/Lib/CertUtil.php
@@ -81,11 +81,13 @@ class CertUtil
     {
         $dec = 0;
         $len = strlen($hex);
-        for ($i = 1; $i <= $len; $i++)
-        {
-            $dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i))));
+        for ($i = 1; $i <= $len; ++$i) {
+            if (ctype_xdigit($hex[$i - 1])) {
+                $dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i))));
+            }
         }
-        return $dec;
+
+        return str_replace('.00', '', $dec);
     }
 
 }
