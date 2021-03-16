b6ad0727f0c26c4eea8bb447ed2ee75a2dcb684f
run0817 <37306014+run0817@users.noreply.github.com>
2021-03-16 19:38:11 +0800

    无效字符过滤 (#25)
    * Update CertUtil.php
    
    * Update CertUtil.php
    
    跳过意外的0x字符
    
    * Update CertUtil.php
    
    跳过意外的字符0x

--- src/Lib/CertUtil.php
+++ src/Lib/CertUtil.php
@@ -81,7 +81,7 @@ class CertUtil
     {
         $dec = 0;
         $len = strlen($hex);
-        for ($i = 1; $i <= $len; $i++)
+        for ($i = 3; $i <= $len; $i++)
         {
             $dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i))));
         }
